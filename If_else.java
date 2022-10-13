97f820780e725686b2b340c0c60c59c40684a481
AdilDev <adilexpo2@gmail.com>
2022-10-13 17:44:11 +0200

    Update If_else.java

--- If_else.java
+++ If_else.java
@@ -1,14 +1,18 @@
 public class If_else {
 
         public static void main(String[] args) {
-            int numeroif = 10;
-            if( numeroif>0)
-            {
-                System.out.println("numeroIf es positivo");
-            } else if (numeroif<0) {
+            int numeroif = 0;
+                
+            if( numeroif<0; { 
+                    System.out.println("numeroIf es positivo");
+            } 
+               else if (numeroif>0) {
                 System.out.println("numeroIf es negativo");
-            }else if(numeroif==0){
+                    
+            }
+               else if(numeroif==0){
                 System.out.println("numeroIf es igual a 0");
+                    
             }else {
                 System.out.println("?");
             }
